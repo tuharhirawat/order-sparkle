957800a469e73af67ff72024bb6929a28cc91496
Tushar <tusharhirawat1918@gmail.com>
2026-09-01 20:58:02 +0100

    Use env variable for API base URL and created vercel.json

--- src/Services/api.ts
+++ src/Services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://localhost:7070/api",
+  baseURL: import.meta.env["VITE_API_URL"] + "/api",
   withCredentials: true,
 });
 
